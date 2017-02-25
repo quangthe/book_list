9d9fe4a0ccc69ccb854ade772b36bfb54e95fbf8
The Tran <the.tran@ext.magnolia-cms.com>
2017-02-25 23:45:00 +0700

    Bind action creator

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {selectBook} from '../actions/index';
+import {bindActionCreators} from 'redux';
 
 class BookList extends Component {
   _renderList() {
     return this.props.books.map((book) => {
       return (
         <li key={book.title}
-            className="list-group-item">
+            className="list-group-item"
+            onClick={() => this.props.selectBook(book)}>
           {book.title}
         </li>
       );
@@ -28,4 +31,12 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+// anything returned from this function will end up as props on the BookList container
+function mapDispatchToProps(dispatch) {
+  // whenever selectBook is called, the result should be passed to all reducers
+  return bindActionCreators({selectBook: selectBook}, dispatch);
+}
+
+// promote BookList from a component to a container - it needs to know about this new dispatch method, selectBook.
+// Make it available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
