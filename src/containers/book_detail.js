b0b55f4f082dd26adf252fe896aa761b1e7d8719
The Tran <the.tran@ext.magnolia-cms.com>
2017-02-26 00:22:18 +0700

    Add BookDetail component and promote it to container

---
+++ src/containers/book_detail.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class BookDetail extends Component {
+  render() {
+    if (!this.props.book) {
+      return (
+        <div>Select a book to get started.</div>
+      );
+    }
+    return (
+      <div>
+        <h3>Detail for:</h3>
+        <div>Title: {this.props.book.title}</div>
+        <div>Page: {this.props.book.page}</div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  // whatever is returned from here will show up as props inside of BookList
+  return {
+    book: state.activeBook
+  };
+}
+
+export default connect(mapStateToProps)(BookDetail);
