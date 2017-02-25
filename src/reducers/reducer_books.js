d0d9fb1766d1420dcb5e97ee644b75b8819c3639
The Tran <the.tran@ext.magnolia-cms.com>
2017-02-25 23:02:54 +0700

    Create book reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+
+export default function() {
+  return [
+    {title: 'Javascript; The Good Parts'},
+    {title: 'Harry Potter'},
+    {title: 'The Dark Tower'},
+    {title: 'Ruby on Rail'}
+  ];
+}
